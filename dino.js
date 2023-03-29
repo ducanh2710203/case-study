8c989f8dc1a5d77a3ca0d384569e4cc2153c8b69
ducanh2710203 <duc2710203@gmail.com>
2023-03-29 16:12:55 +0700

    first commit

---
+++ dino.js
@@ -0,0 +1,9 @@
+class Dino {
+    constructor(x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+
+    }
+}
--- game.js
+++ game.js
@@ -2,30 +2,13 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 const audio = document.querySelector(".audio");
 const audio1 = document.querySelector(".audio1");
+const audio3 = document.querySelector(".audio2");;
 
-
-class Dino {
-    constructor(x, y, width, height) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-
-    }
-}
-
-class Food {
-    constructor(x, y, width, height) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-    }
-}
 
 let dino = new Dino(100, 100, 70, 90)
 let food = new Food(400, 400, 50, 50)
-let food1 = new Food(Math.random() * 400, Math.random() * 400, 50, 50)
+let bom1 = new Food(Math.random() * 400, Math.random() * 400, 50, 60)
+let bom2 = new Food(Math.random() * 400, Math.random() * 400, 50, 60)
 var score = 0;
 
 function drawDino() {
@@ -47,13 +30,14 @@ function drawFood() {
     img.src = "thit.png"
 }
 
-function drawObstacle() {
+function drawObstacle(bom1) {
     let img = new Image()
     img.onload = () => {
-        this.context.drawImage(img, food1.x, food1.y, food1.width, food1.height)
+        this.context.drawImage(img, bom1.x, bom1.y, bom1.width, bom1.height)
 
     }
     img.src = "bom.png"
+
 }
 
 function end() {
@@ -73,12 +57,24 @@ function update() {
         score++;
         food.x = Math.random() * (canvas.width - food.width);
         food.y = Math.random() * (canvas.height - food.height);
+        bom1.x = Math.random() * (canvas.width - bom1.width);
+        bom1.y = Math.random() * (canvas.height - bom1.height);
+        bom2.x = Math.random() * (canvas.width - bom2 .width);
+        bom2.y = Math.random() * (canvas.height - bom2.height);
 
     }
-    if (dino.x <= food1.x + food1.width && dino.x + dino.width >= food1.x && dino.y <= food1.y + food1.height && dino.y + dino.height >= food1.y) {
+    if (dino.x < bom1.x + bom1.width && dino.x + dino.width > bom1.x && dino.y < bom1.y + bom1.height && dino.y + dino.height > bom1.y) {
 
         end()
-        // alert("bạn đã thua")
+        alert("bạn đã thua")
+        clearInterval(set)
+        audio.pause()
+        audio1.play()
+    }
+    if (dino.x < bom2.x + bom2.width && dino.x + dino.width > bom2.x && dino.y < bom2.y + bom2.height && dino.y + dino.height > bom2.y) {
+
+        end()
+        alert("bạn đã thua")
         clearInterval(set)
         audio.pause()
         audio1.play()
@@ -86,24 +82,28 @@ function update() {
 
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawObstacle();
+    drawObstacle(bom1);
+    drawObstacle(bom2);
     drawDino();
     drawFood();
-    if (score < 20) {
-        set
+    if (score < 10) {
     } else {
-            end()
+        end()
         context.fillStyle = 'red';
         context.font = '20px Arial';
-        context.fillText("you win", 50, 50)
+        context.fillText("you win", 500, 50)
+        audio3.play()
+
 
         clearInterval(set)
         audio.pause()
     }
     context.fillStyle = 'red';
     context.font = '20px Arial';
-    context.fillText('Score: ' + score, 10, 30);
+    context.fillText('Score: ' + score, 500, 30);
 }
+
+// update()
 
 var set
 
@@ -112,15 +112,37 @@ function star() {
 
     update()
     score = 0
-    set = setInterval(update, 100);
+    set = setInterval(update, 15)
     food.x = Math.random() * (canvas.width - food.width);
     food.y = Math.random() * (canvas.height - food.height);
-    food1.x = Math.random() * (canvas.width - food1.width);
-    food1.y = Math.random() * (canvas.height - food1.height);
+    bom1.x = Math.random() * (canvas.width - bom1.width);
+    bom1.y = Math.random() * (canvas.height - bom1.height);
+    bom2.x = Math.random() * (canvas.width - bom2.width);
+    bom2.y = Math.random() * (canvas.height - bom2.height);
 }
 
 function End() {
     end()
+    audio.pause()
+    audio1.pause()
+    audio3.pause()
     clearInterval(set)
 
+
 }
+const fireworksContainer = document.getElementById('fireworks-container');
+const fireworksButton = document.getElementById('fireworks-button');
+
+function launchFireworks() {
+    const firework = document.createElement('div');
+    firework.classList.add('firework');
+    firework.style.left = Math.random() * 100 + '%';
+    firework.style.top = Math.random() * 100 + '%';
+    fireworksContainer.appendChild(firework);
+}
+
+fireworksButton.addEventListener('click', function() {
+    for (let i = 0; i < 30; i++) {
+        setTimeout(launchFireworks, i * 100);
+    }
+});
